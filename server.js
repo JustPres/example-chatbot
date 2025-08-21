0af92cf494462c15a54a4603cff1609dd59431c4
Justine Lopez <justine.personal.space@gmail.com>
2025-08-21 16:24:24 +0800

    s

--- server.js
+++ server.js
@@ -42,6 +42,12 @@ app.post("/webhook", async (req, res) => {
     }
 });
 
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
