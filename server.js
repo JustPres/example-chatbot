9689fb625f882a3feace3d6e46d77dd443484f65
Justine Lopez <justine.personal.space@gmail.com>
2025-08-21 16:07:27 +0800

    reconfigure webhook api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
-require("dotenv").config();
+//require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
